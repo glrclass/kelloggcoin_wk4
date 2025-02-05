1b0d34f443a18d22a8efc079fadc160a270f0f8a
G R <gabi.romagnoli@kellogg.northwestern.edu>
2025-02-05 23:44:23 +0000

    still working through this

--- practice.rb
+++ practice.rb
@@ -27,3 +27,52 @@ blockchain = [
 # Anthony's KelloggCoin balance is 2650
 
 # 👇👇👇 Your code HERE 👇👇👇
+
+index = 0
+friend = friends[index]
+
+counter = 0
+loop do 
+  if counter == 5
+    break
+  end
+
+
+me = { "name" => "Ben", "location" => "Chicago", "status" => "ENTR-924 at Kellogg!" };
+puts me
+
+# Accessing data from the hash
+
+# Now that the hash is defined, we can access the attributes:
+name = me["name"]
+puts name # Ben
+
+location = me["location"]
+puts location # Chicago
+
+--
+bank_account_balance = 51
+if bank_account_balance > 50
+  bank_account_balance = bank_account_balance - 50
+  puts "You have successfully withdrawn $50. New balance is $#{bank_account_balance}."
+else
+  puts "Insufficient funds for that transaction."
+end
+
+# Elsif Conditional Logic
+your_team_score = 2
+other_team_score = 2
+if your_team_score > other_team_score
+  puts "Yay, your team won!"
+elsif your_team_score == other_team_score
+  puts "Ok, you tied ¯\_(ツ)_/¯"
+else
+  puts "Your team lost :("
+end
+
+# Combining Expressions
+temp = 68
+precipitation = 0
+if temp >= 65 && temp <= 75 && precipitation == 0
+  puts "It's perfect outside!"
+end
